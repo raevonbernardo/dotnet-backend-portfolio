92e8f3bcfdc3b78b75a6f7bab6ee83c44423a4d7
raevon <raevonbernardo.gh@gmail.com>
2026-05-05 19:20:57 +0800

    Backend: Integrate Scalar for API testing

--- RTBackendAPI/RTBackendAPI/Program.cs
+++ RTBackendAPI/RTBackendAPI/Program.cs
@@ -1,8 +1,11 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,6 +13,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseSwagger();
 }
 
 app.UseHttpsRedirection();
