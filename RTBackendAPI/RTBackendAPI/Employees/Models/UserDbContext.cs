6b3155fd79c5de476936e2dc536b0c27f274c599
raevon <raevonbernardo.gh@gmail.com>
2026-05-06 20:58:39 +0800

    Backend: Add UserDbContext to store User data

---
+++ RTBackendAPI/RTBackendAPI/Employees/Models/UserDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RTBackendAPI.Employees.Models;
+
+public sealed class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
