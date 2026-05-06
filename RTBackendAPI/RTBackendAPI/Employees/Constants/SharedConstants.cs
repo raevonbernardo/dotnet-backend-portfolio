8380991dabded0998d55fff5f79045f241e436ff
raevon <raevonbernardo.gh@gmail.com>
2026-05-06 20:56:49 +0800

    Backend: Add SharedConstants

---
+++ RTBackendAPI/RTBackendAPI/Employees/Constants/SharedConstants.cs
@@ -0,0 +1,12 @@
+namespace RTBackendAPI.Employees.Constants;
+
+public static class SharedConstants
+{
+    public const int MIN_USERNAME_LENGTH = 4;
+    public const int MAX_USERNAME_LENGTH = 255;
+    
+    public const int MIN_PASSWORD_LENGTH = 8;
+    public const int MAX_PASSWORD_LENGTH = 30;
+    
+    public const int MAX_PASSWORD_HASHED_LENGTH = 200;
+}
