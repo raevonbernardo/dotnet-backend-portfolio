7ef08142b9cc578e0d161e82ea7e1992c0135e65
raevon <raevonbernardo.gh@gmail.com>
2026-05-07 20:05:25 +0800

    Backend: Add EmployeeDbContext migration

---
+++ RTBackendAPI/RTBackendAPI/Migrations/EmployeeDb/20260507120306_InitialCreateEmployeeDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RTBackendAPI.Migrations.EmployeeDb
+{
+    /// <inheritdoc />
+    public partial class InitialCreateEmployeeDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    PrivateId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployeeId = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Job = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    EmploymentDateTimestamp = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.PrivateId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
