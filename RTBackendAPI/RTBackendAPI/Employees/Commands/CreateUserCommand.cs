3c25c05a82f115a858b8b8e1c2f2244842d74a60
raevon <raevonbernardo.gh@gmail.com>
2026-05-08 07:47:08 +0800

    Backend: Move validation of CreateUserCommand to RuleBuilderExtensions

--- RTBackendAPI/RTBackendAPI/Employees/Commands/CreateUserCommand.cs
+++ RTBackendAPI/RTBackendAPI/Employees/Commands/CreateUserCommand.cs
@@ -1,6 +1,5 @@
 using System.Text.Json.Serialization;
 using FluentValidation;
-using RTBackendAPI.Employees.Constants;
 using RTBackendAPI.Employees.Extensions;
 using RTBackendAPI.Employees.Services;
 
@@ -33,30 +32,13 @@ public sealed class CreateUserCommandValidator : AbstractValidator<CreateUserCom
     public CreateUserCommandValidator(IConfigManager configManager)
     {
         RuleFor(command => command.Username)
-            .NotNullOrEmpty()
-            .WithMessage("{PropertName} cannot be empty.")
-            .MinimumLength(SharedConstants.MIN_USERNAME_LENGTH)
-            .WithMessage($"{{PropertName}} should contain at least {SharedConstants.MIN_USERNAME_LENGTH} characters.")
-            .MaximumLength(SharedConstants.MAX_USERNAME_LENGTH)
-            .WithMessage($"Max limit for {{PropertName}} is {SharedConstants.MAX_USERNAME_LENGTH}.")
-            .IsAlphanumericOnly()
-            .WithMessage("{PropertyName} should only contain alphanumeric characters.");
-        
+            .ValidateUsername();
+
         RuleFor(command => command.Password)
-            .Must(password => !string.IsNullOrWhiteSpace(password))
-            .WithMessage("{PropertName} cannot be empty.")
-            .MinimumLength(SharedConstants.MIN_PASSWORD_LENGTH)
-            .WithMessage($"{{PropertName}} should contain at least {SharedConstants.MIN_PASSWORD_LENGTH} characters.")
-            .MaximumLength(SharedConstants.MAX_PASSWORD_LENGTH)
-            .WithMessage($"Max limit for {{PropertName}} is {SharedConstants.MAX_PASSWORD_LENGTH}.")
-            .HasNoSpaces()
-            .WithMessage("{PropertyName} should contain not contain spaces.");
+            .ValidatePassword();
 
         RuleFor(command => command.ApiKey)
-            .NotNull()
-            .NotEmpty()
-            .Must(apiKey => string.Equals(apiKey, configManager.ApiKey()))
-            .WithMessage("Invalid api key.");
+            .ValidateApiKey(configManager);
     }
 }
 
--- RTBackendAPI/RTBackendAPI/Employees/Extensions/RuleBuilderExtensions.cs
+++ RTBackendAPI/RTBackendAPI/Employees/Extensions/RuleBuilderExtensions.cs
@@ -1,4 +1,6 @@
 using FluentValidation;
+using RTBackendAPI.Employees.Constants;
+using RTBackendAPI.Employees.Services;
 
 namespace RTBackendAPI.Employees.Extensions;
 
@@ -17,5 +19,49 @@ public static class RuleBuilderExtensions
     public static IRuleBuilderOptions<T, string> HasNoSpaces<T>(this IRuleBuilder<T, string> builder)
     {
         return builder.Must(value => !value.Contains(' '));
+    }
+    
+    public static IRuleBuilderOptions<T, string> ValidateCurrentUsername<T>(this IRuleBuilder<T, string> builder)
+    {
+        return builder
+            .NotNullOrEmpty()
+            .MaximumLength(SharedConstants.MAX_USERNAME_LENGTH)
+            .IsAlphanumericOnly()
+            .WithMessage("Invalid {PropertyName}.");
+    }
+
+    public static IRuleBuilderOptions<T, string> ValidateUsername<T>(this IRuleBuilder<T, string> builder)
+    {
+        return builder
+            .NotNullOrEmpty()
+            .WithMessage("{PropertName} cannot be empty.")
+            .MinimumLength(SharedConstants.MIN_USERNAME_LENGTH)
+            .WithMessage($"{{PropertName}} should contain at least {SharedConstants.MIN_USERNAME_LENGTH} characters.")
+            .MaximumLength(SharedConstants.MAX_USERNAME_LENGTH)
+            .WithMessage($"Max limit for {{PropertName}} is {SharedConstants.MAX_USERNAME_LENGTH}.")
+            .IsAlphanumericOnly()
+            .WithMessage("{PropertyName} should only contain alphanumeric characters.");
+    }
+    
+    public static IRuleBuilderOptions<T, string> ValidatePassword<T>(this IRuleBuilder<T, string> builder)
+    {
+        return builder
+            .NotNullOrEmpty()
+            .WithMessage("{PropertName} cannot be empty.")
+            .MinimumLength(SharedConstants.MIN_PASSWORD_LENGTH)
+            .WithMessage($"{{PropertName}} should contain at least {SharedConstants.MIN_PASSWORD_LENGTH} characters.")
+            .MaximumLength(SharedConstants.MAX_PASSWORD_LENGTH)
+            .WithMessage($"Max limit for {{PropertName}} is {SharedConstants.MAX_PASSWORD_LENGTH}.")
+            .HasNoSpaces()
+            .WithMessage("{PropertyName} should contain not contain spaces.");
+    }
+    
+    public static IRuleBuilderOptions<T, string> ValidateApiKey<T>(this IRuleBuilder<T, string> builder, 
+        IConfigManager configManager)
+    {
+        return builder
+            .NotNullOrEmpty()
+            .Must(apiKey => string.Equals(apiKey, configManager.ApiKey()))
+            .WithMessage("Invalid api key.");
     }
 }
