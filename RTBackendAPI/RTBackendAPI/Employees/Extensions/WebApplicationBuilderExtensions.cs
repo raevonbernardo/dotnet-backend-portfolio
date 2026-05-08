d2a88a316992121445518cf832fd38ed82ece743
raevon <raevonbernardo.gh@gmail.com>
2026-05-08 10:39:36 +0800

    Backend: Don't save jwt bearer tokens

--- RTBackendAPI/RTBackendAPI/Employees/Extensions/WebApplicationBuilderExtensions.cs
+++ RTBackendAPI/RTBackendAPI/Employees/Extensions/WebApplicationBuilderExtensions.cs
@@ -54,7 +54,6 @@ public static class WebApplicationBuilderExtensions
             })
             .AddJwtBearer(options =>
             {
-                options.SaveToken = true;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
